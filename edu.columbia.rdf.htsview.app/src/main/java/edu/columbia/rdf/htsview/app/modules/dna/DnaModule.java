c69273cfd37709bd995032523bc78ef84cb3d0ba
Antony Holmes <antony.b.holmes@gmail.com>
2018-01-19 20:31:07 -0500

    Updates and bug fixes.

--- edu.columbia.rdf.htsview.app/src/main/java/edu/columbia/rdf/htsview/app/modules/dna/DnaModule.java
+++ edu.columbia.rdf.htsview.app/src/main/java/edu/columbia/rdf/htsview/app/modules/dna/DnaModule.java
@@ -32,7 +32,7 @@ import org.jebtk.modern.button.ModernButton;
 import org.jebtk.modern.event.ModernClickEvent;
 import org.jebtk.modern.event.ModernClickListener;
 import org.jebtk.modern.ribbon.RibbonLargeButton;
-import org.jebtk.modern.widget.tooltip.ModernToolTip;
+import org.jebtk.modern.tooltip.ModernToolTip;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
